fd81846caf932d1e3d742f4b39d393629d7880a2
Dread <dread@start9labs.com>
2022-09-09 14:47:26 -0400

    works™

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,11 @@ RUN apt-get update && apt-get install wget curl sudo bash tini -y \
         tar xz && mv yq_linux_arm /usr/bin/yq
 
 ENV APP_HOST robosats.embassy
-ENV APP_PORT 9000
-ENV TOR_PROXY_IP robosats.embassy
+ENV APP_PORT 12596
+ENV TOR_PROXY_IP embassy
 ENV TOR_PROXY_PORT 9050
 
 ADD ./docker_entrypoint.sh /usr/local/bin/docker_entrypoint.sh
 ADD check-web.sh /usr/local/bin/check-web.sh
 RUN chmod a+x /usr/local/bin/*.sh
 RUN chmod a+x *.sh
-
-EXPOSE 12596
-
-ENTRYPOINT ["/usr/local/bin/docker_entrypoint.sh"]
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-set -e
-
-# Setting env-vars from config page
-echo "Setting environment variables..."
-export ROBOSATS_ONION=$(yq e '.tor-address' /root/start9/config.yaml)
-export LAN_HOST=$(yq e '.lan-address' /root/start9/config.yaml)
-# export TOR_PROXY_IP=$(yq e '.tor-address' /root/start9/config.yaml)
-client_server="npm exec http-server -- . -p 12596 -P http://127.18.0.1:9050 --cors -i false -d false"
-backend_tor_bridge="socat tcp4-LISTEN:81,reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:${TOR_PROXY_IP:-127.18.0.1}:${ROBOSATS_ONION:-robosats6tkf3eva7x2voqso3a5wcorsnw34jveyxfqi2fu7oyheasid.onion}:80,socksport=${TOR_PROXY_PORT:-9050}"
+set -ea
 
 echo "Starting RoboSats..."
-exec tini $client_server & $backend_tor_bridge & nginx -g "daemon off;"
-
+exec tini -s ./robosats-client.sh 
