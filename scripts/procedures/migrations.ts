442d82accd08466b8c2e8cfdf52175d9f43bd65f
kn0wmad <39687477+kn0wmad@users.noreply.github.com>
2023-03-09 22:35:33 +0000

    0.4.0 update (#7)
    * Init 0.4.0 update
    
    * Stop DDOSing Robosats coordinator with hcks

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "0.3.3" );
+    .fromMapping({}, "0.4.0" );
