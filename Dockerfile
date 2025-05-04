f9c5a41c45d369cf76a010003629c55bae0f1f1d
koalasat <koalasat@satstralia.com>
2025-05-04 18:34:52 +0200

    v0.7.7-alpha

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM recksato/robosats-client:v0.7.6-alpha
+FROM recksato/robosats-client:v0.7.7-alpha
 RUN apk add bash curl sudo tini wget yq; \
     rm -f /var/cache/apk/*
 
--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration =
-  compat.migrations.fromMapping({}, "0.7.6");
+  compat.migrations.fromMapping({}, "0.7.7");
